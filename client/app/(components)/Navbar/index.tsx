d5bbd30d85ddcaa0ddfb85064ce3aca868460655
Chahin Boudra <boudrachahin@gmail.com>
2026-08-06 19:04:53 +0100

    feat: integrate Redux Toolkit for state management with dark mode and sidebar collapse functionality

--- client/app/(components)/Navbar/index.tsx
+++ client/app/(components)/Navbar/index.tsx
@@ -1,8 +1,19 @@
-import { Search, Settings } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Menu, Moon, Search, Settings, Sun } from "lucide-react";
 import Link  from "next/link";
 
 
 const Navbar = () => {
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => 
+      state.global.isSidebarCollapsed,
+    );
+  const isDarkMode = useAppSelector((state) => 
+    state.global.isDarkMode,
+  );  
+
   return (
     <div className='
       flex 
@@ -13,7 +24,15 @@ const Navbar = () => {
       py-3 
       dark:bg-black
     '>
+      {/* searchbarCollaped */}
       <div className='flex items-center gap-8'>
+
+      {!isSidebarCollapsed ? null : (
+        <button onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}>
+          <Menu className="h-8 w-8 dark:text-white" />
+        </button>
+      )}
+
           <div className='relative flex h-min w-[200px]'>
             <Search className="
               absolute 
@@ -47,9 +66,22 @@ const Navbar = () => {
           </div>
       </div>
       <div className="flex items-center">
+        <button onClick={() => dispatch(setIsDarkMode(!isDarkMode))} 
+          className={isDarkMode 
+            ? `rounded p-2 dark:hover:bg-gray-700` 
+            : `rounded p-2 hover:bg-gray-100 `}  
+        >
+          {isDarkMode ? (
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+          ) : (
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          )}
+        </button>
         <Link
           href="/settings"
-          className="h-min w-min rounded p-2 hover:bg-gray-100"
+          className={isDarkMode 
+            ? `h-min w-min rounded p-2 dark:hover:bg-gray-700` 
+            : `h-min w-min rounded p-2 hover:bg-gray-100 `}  
         >
           <Settings
             className="
